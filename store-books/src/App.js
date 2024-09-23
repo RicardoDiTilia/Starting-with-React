09f76e20b778ce8d08024d2aaf11932cdb3076ea
Ricardo Cerazi Di Tilia <riiditilia@gmail.com>
2024-09-23 09:46:26 -0300

    Starting with react

---
+++ store-books/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='logo'>
+          <img src={logo} alt="logo"></img>
+          <p><strong>Store</strong>Books</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
